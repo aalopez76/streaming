542e386d1f62314ecae1a5952c49297d074ea264
aalopez76 <aalpzp@gmail.com>
2024-05-24 03:39:23 +0000

    release version 1.0

--- movies_app.py
+++ movies_app.py
@@ -7,7 +7,7 @@ import json
 # Conexión a Firestore usando credenciales JSON
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project="Streaming")
+db = firestore.Client(credentials=creds, project="streaming-4e657")
 
 dbMovies = db.collection('movies')
 
